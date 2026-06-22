65ad776b0cc9e36160f604fb126ec045d03a600e
Muhammad Ghifari <muhammadghifari37@gmail.com>
2026-06-22 19:52:45 +0700

    ganti dollar ke rupiah (fixed 3)

--- app.py
+++ app.py
@@ -624,7 +624,7 @@ with tab6:
     with sim_left:
         hourly_cost = st.slider(
             "Estimasi biaya per server per jam (Rupiah):",
-            min_value=100.0, max_value=15000.0, value=500.0, step=50,
+            min_value=100.0, max_value=15000.0, value=500.0, step=50.0,
             help="Biaya gabungan termasuk komputasi, penyimpanan, lisensi, dan overhead operasional."
         )
 
